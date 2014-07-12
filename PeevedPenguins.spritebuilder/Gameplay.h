fa83fe73b926f7011285c6c9efbdfbf5b89772b5
Napat Boonsaeng <napat2000@gmail.com>
2014-07-11 21:08:35 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Napat Boonsaeng on 7/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
