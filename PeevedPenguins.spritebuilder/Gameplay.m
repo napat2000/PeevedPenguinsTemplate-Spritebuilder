0d0797d0648db30108e69f6afff53f5b19245176
Napat Boonsaeng <napat2000@gmail.com>
2014-07-11 23:27:14 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -21,6 +21,7 @@
     self.userInteractionEnabled = TRUE;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    _physicsNode.debugDraw = TRUE;
 }
 
 // called on every touch in this scene
